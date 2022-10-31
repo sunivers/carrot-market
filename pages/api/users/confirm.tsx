25ad1d5ce3fd9586997b123ab712f8c4628dc885
sunivers <soyoungp02@gmail.com>
2022-10-31 23:31:59 +0900

    add iron-session for session

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandlers";
 import client from "@libs/server/client";
@@ -7,8 +8,23 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log("token", token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    include: { user: true }, // include를 이용해 foreign key로 연결된 관계 객체를 가져올 수 있음
+  });
+  console.log(exists);
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
+  if (!exists) res.status(404).end();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "i4ui2u3id8fu2i0fji039r22389udis39sjodjo39odoir0923moeru3oj0w9r23idj83pdoede9si03jdos9ef0s",
+});
