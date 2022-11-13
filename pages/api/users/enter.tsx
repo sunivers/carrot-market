2fc12dba50ed9c2a7337339bd8c8a5b92f39fb1a
sunivers <soyoungp02@gmail.com>
2022-11-13 18:29:16 +0900

    protected handlers

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -66,4 +66,4 @@ async function handler(
 }
 
 // NextJS에서 api를 실행시키려면 반드시 함수를 반환해야함 (withHandler 내부 구현을 보면 function을 반환하고 있음)
-export default withHandler("POST", handler);
+export default withHandler({ method: "POST", handler, isPrivate: false });
