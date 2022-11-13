5e8b7a977dd578dbe3300deae694b4508c5d4fab
sunivers <soyoungp02@gmail.com>
2022-11-13 20:35:10 +0900

    Add product upload form

--- components/input.tsx
+++ components/input.tsx
@@ -7,6 +7,7 @@ interface InputProps {
   required: boolean;
   kind?: "text" | "phone" | "price";
   register: UseFormRegisterReturn;
+  [key: string]: any;
 }
 
 export default function Input({
@@ -16,6 +17,7 @@ export default function Input({
   required,
   kind = "text",
   register,
+  ...rest
 }: InputProps) {
   return (
     <div>
@@ -32,6 +34,7 @@ export default function Input({
             required={required}
             type={type}
             {...register}
+            {...rest}
             className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
         </div>
@@ -46,6 +49,7 @@ export default function Input({
             required={required}
             type={type}
             {...register}
+            {...rest}
             className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
           <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
@@ -63,6 +67,7 @@ export default function Input({
             required={required}
             type={type}
             {...register}
+            {...rest}
             className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
         </div>
