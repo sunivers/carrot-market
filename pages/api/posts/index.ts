625005edcb335d19d40521603b356be1228a750d
sunivers <soyoungp02@gmail.com>
2023-01-15 16:09:21 +0900

    All posts

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -7,25 +7,53 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const {
-    body: { question },
-    session: { user },
-  } = req;
-  const post = await client.post.create({
-    data: {
-      question,
-      user: {
-        connect: {
-          id: user?.id,
+  if (req.method === "GET") {
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            interest: true,
+            answers: true,
+          },
         },
       },
-    },
-  });
+    });
+    res.json({
+      ok: true,
+      posts,
+    });
+  }
 
-  res.json({
-    ok: true,
-    post,
-  });
+  if (req.method === "POST") {
+    const {
+      body: { question },
+      session: { user },
+    } = req;
+    const post = await client.post.create({
+      data: {
+        question,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+
+    res.json({
+      ok: true,
+      post,
+    });
+  }
 }
 
-export default withApiSession(withHandler({ method: ["POST"], handler }));
+export default withApiSession(
+  withHandler({ method: ["GET", "POST"], handler })
+);
