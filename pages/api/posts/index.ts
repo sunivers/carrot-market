f769dc994de96ed6746e75aaaaa8e98e17dd8d71
sunivers <soyoungp02@gmail.com>
2023-01-17 10:48:37 +0900

    post filtering by coords

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -2,13 +2,34 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandlers";
 import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
+import { DEFAULT_COORDS_RANGE } from "@libs/server/constants";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   if (req.method === "GET") {
+    const {
+      query: { latitude, longitude },
+    } = req;
+    const parsedLatitude = parseFloat(latitude?.toString() || "0");
+    const parsedLongitude = parseFloat(longitude?.toString() || "0");
+
+    if (!parsedLatitude || !parsedLongitude) {
+      res.status(400);
+    }
+
     const posts = await client.post.findMany({
+      where: {
+        latitude: {
+          gte: parsedLatitude - DEFAULT_COORDS_RANGE,
+          lte: parsedLatitude + DEFAULT_COORDS_RANGE,
+        },
+        longitude: {
+          gte: parsedLongitude - DEFAULT_COORDS_RANGE,
+          lte: parsedLongitude + DEFAULT_COORDS_RANGE,
+        },
+      },
       include: {
         user: {
           select: {
